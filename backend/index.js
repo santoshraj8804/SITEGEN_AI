aad9b030c4a8fad61f29c5e3ea1176b05b868791
Santosh raj <santoshrajgupta7366@gmail.com>
2026-07-01 20:48:02 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import 'dotenv/config'
+import connectDB from "./database/db.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port:${PORT}`);
+});
