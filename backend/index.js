56c0956d6c82c82e244054bca55cdd3a2c258946
Santosh raj <santoshrajgupta7366@gmail.com>
2026-07-02 10:46:52 +0530

    Frontend setup

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
 import express from "express";
 import 'dotenv/config'
 import connectDB from "./database/db.js";
+import authRoute from "./routes/authRoute.js";
+import cookieParser from 'cookie-parser'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/auth', authRoute);
 
 app.listen(PORT, () => {
     connectDB();
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-red-500 font-bold text-5xl'>
+      Hello World
+    </div>
+  )
+}
+
+export default App
