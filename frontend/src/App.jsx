a7a3b48eb7abd6b50eb8366865d9501cbf997124
Santosh raj <santoshrajgupta7366@gmail.com>
2026-07-05 19:36:33 +0530

    Create Navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Pricing from './pages/Pricing'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<Home/>}/>
+        <Route path="/pricing" element={<Pricing/>} />
       </Routes>
     </BrowserRouter>
   )
