f93444c6cb181da7ead1e16e4283e3520e4289aa
Santosh raj <santoshrajgupta7366@gmail.com>
2026-07-07 17:34:16 +0530

    Add website api routes for all controller

--- backend/routes/websiteRoute.js
+++ backend/routes/websiteRoute.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import { isAuthenticated } from '../middlewares/isAuthenticated.js'
-import { generateWebsite } from '../controllers/websiteController.js'
+import { generateWebsite, changeWebsite, getWebsiteById, getAllWebsite, deployWebsite, getBySlug } from '../controllers/websiteController.js'
 
 
 const router = express.Router()
 
 router.post('/generate', isAuthenticated, generateWebsite)
+router.post('/update/:id', isAuthenticated, changeWebsite)
+router.get('/getbyid/:id', isAuthenticated, getWebsiteById)
+router.get('/getall', isAuthenticated, getAllWebsite)
+router.get('/deploy/:id', isAuthenticated, deployWebsite)
+router.get('/getbyslug/:slug', isAuthenticated, getBySlug)
 
 export default router
